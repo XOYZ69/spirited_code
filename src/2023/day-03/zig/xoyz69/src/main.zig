5e330936fb495cb118336fa94ebb253ef49dabe1
Kevin Pagenkämper <k.pagenkaemper@leannova.de>
2024-08-15 12:54:42 +0200

    Fixed the segmentation fault error

--- src/2023/day-03/zig/xoyz69/src/main.zig
+++ src/2023/day-03/zig/xoyz69/src/main.zig
@@ -14,12 +14,11 @@ const Coord = struct {
 };
 
 fn is_valid_character(char: u8) bool {
-
     if (char == '\n') {
         return false;
     }
 
-    for (invalid_symbols) | symbol | {
+    for (invalid_symbols) |symbol| {
         if (symbol == char) {
             return false;
         }
@@ -48,24 +47,22 @@ pub fn main() !void {
 
     var line_index: u8 = 0;
 
-    var cache_hash_map = std.AutoHashMap(usize, std.ArrayList(usize)).init(allocator);
+    var cache_parent_array = std.ArrayList(std.ArrayList(usize)).init(allocator);
 
     while (reader.streamUntilDelimiter(writer, '\n', null)) {
+        var object_position = std.ArrayList(usize).init(allocator);
         defer line.clearRetainingCapacity();
 
-        var object_position = std.ArrayList(usize).init(allocator);
-        defer object_position.deinit();
-
         std.debug.print("{d} -> ", .{line_index});
-        for (line.items[0..line.items.len - 2 ], 0..) |item, i| {
+        for (line.items[0 .. line.items.len - 2], 0..) |item, i| {
             if (!is_valid_character(item)) {
                 continue;
             }
             try object_position.append(i);
-            std.debug.print("[{d}: {c}]", .{i, item});
+            std.debug.print("[{d}: {c}]", .{ i, item });
         }
-        try cache_hash_map.put(line_index, object_position);
-        // std.debug.print("\n HM --> {any}", .{cache_hash_map.get(line_index)});
+
+        try cache_parent_array.append(object_position);
         std.debug.print("\n", .{});
 
         line_index += 1;
@@ -76,17 +73,18 @@ pub fn main() !void {
 
     while (line_index > 0) {
         line_index -= 1;
-        // const current_list = std.ArrayList(usize).init(allocator);
-        const current_list_opt = &cache_hash_map.get(line_index);
 
-        // defer current_list.deinit();
-        if (current_list_opt) |current_list| {
-            std.debug.print("{d}: {s}\n", .{ line_index, &current_list.items });
-        } else {
-            std.debug.print("{d}: null\n", .{ line_index });
+        const current_list_opt = cache_parent_array.items[line_index];
+
+        std.debug.print("{d}: ", .{line_index});
+
+        for (current_list_opt.items) |item| {
+            std.debug.print("{d} ", .{item});
         }
+
+        current_list_opt.deinit();
+        std.debug.print("\n", .{});
     }
 
-    cache_hash_map.deinit();
-    // std.debug.print("List: {s}", .{list.items});
+    cache_parent_array.deinit();
 }
