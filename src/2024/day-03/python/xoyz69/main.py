056a8fca6da2e4793e699f6d8fcc7dae87f2607c
XOYZ69 <xoyz.productions@gmail.com>
2024-12-05 21:57:04 +0100

    feat: Day 3

--- src/2024/day-03/python/xoyz69/main.py
+++ src/2024/day-03/python/xoyz69/main.py
@@ -15,24 +15,21 @@ def part_2(lines):
     last = 'do'
     for line in lines:
         for match in re.finditer(r'mul\((0|[1-9][0-9]*),(0|[1-9][0-9]*)\)|do\(\)|don\'t\(\)', line):
-            # print(matches)
-            # print(match.span())
             if 'do()' in match.group():
                 last = 'do'
+                continue
             elif 'don\'t()' in match.group():
                 last = 'dont'
-            print(match.group(), last, total, end=' ')
+                continue
 
             if last == 'do':
-                print('do')
-                total += int(match.span()[0]) * int(match.span()[1])
-            else:
-                print('dont')
+                cache = match.group().split(',')
+                total += int(cache[0].split('(')[1]) * int(cache[1].replace(')', ''))
 
     print('Total part 2:', total)
 
 def main():
-    with open('../../input', 'r', encoding='utf-8') as input_read:
+    with open('../../.input', 'r', encoding='utf-8') as input_read:
         lines = input_read.readlines()
 
     part_1(lines)
