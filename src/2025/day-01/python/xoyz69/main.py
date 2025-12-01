66c6f9fa491006898b7c410cd94f39e96bd00877
Kevin Pagenkämper <kp@maxbenedikt.com>
2025-12-01 13:52:12 +0100

    feat: wip add part02 solution for day 01

--- src/2025/day-01/python/xoyz69/main.py
+++ src/2025/day-01/python/xoyz69/main.py
@@ -10,16 +10,10 @@ def part01():
     with open("input", "r") as input_file:
         operations = input_file.readlines()
 
-    print(len(operations))
+    print("Input length", len(operations))
 
     for op in operations:
-        if op.startswith("L"):
-            direction = 1
-        elif op.startswith("R"):
-            direction = -1
-        else:
-            direction = 1
-            print(f"Line could not be handled: [{op}]")
+        direction = 1 if op.startswith("L") else -1
 
         steps = int(op[1:])
 
@@ -31,5 +25,35 @@ def part01():
     print(f"Score: {score}")
 
 
+def part02():
+    print("2025 - Day 01 - part 02")
+
+    dial_pointer = 50
+    score = 0
+
+    with open("input", "r") as input_file:
+        operations = input_file.readlines()
+
+    print("Input length", len(operations))
+
+    for op in operations:
+        direction = 1 if op.startswith("L") else -1
+
+        steps = int(op[1:])
+
+        dial_pointer_prev = dial_pointer
+        turn = dial_pointer - (steps * direction)
+        dial_pointer = turn % len(dial)
+
+        score += int(abs(turn) / len(dial))
+
+        score += 1 if turn < 0 else 0
+
+    print(f"Score: {score}")
+
+
 if __name__ == "__main__":
+    print("--------------")
     part01()
+    print("--------------")
+    part02()
